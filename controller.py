f9539c45bf38ad90b61f904834cc4f707c25573b
n-chicken <nether_chicken@hotmail.com>
2022-01-23 10:17:49 -0300

    Messing with embeds in auction list

--- controller.py
+++ controller.py
@@ -24,13 +24,14 @@ BOT_TOKEN_KEY = 'MTA_EXCHANGE_DISCORD_BOT_TOKEN'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MTA Exchange v2 Bot')
-    parser.add_argument('token', nargs='?', default=os.environ.get(BOT_TOKEN_KEY))
+    parser.add_argument('token', nargs='?',
+                        default=os.environ.get(BOT_TOKEN_KEY))
     parser.add_argument('-g', '--guild', type=int, default=815757801133441115)
     args = parser.parse_args()
 else:
     sys.exit(1)
 
-bot = commands.Bot(command_prefix="$", test_guilds=[args.guild])
+bot = commands.Bot(test_guilds=[args.guild])
 
 items = json.load(open('data/minecraft-items.json'))
 
@@ -38,49 +39,86 @@ signal_service = SignalService()
 
 # -------------------------------------------------------------
 
-# Events
+
+def create_embed(auction: Auction) -> disnake.Embed:
+    is_buy = auction.intention == 'buy'
+    item = auction.item_recv if is_buy else self.auction
+    embed = disnake.Embed(
+        title='Buying' if is_buy else 'Selling',
+        description=item,
+        colour=disnake.Colour.white() if is_buy else disnake.Colour.green()
+    )
+    file = disnake.File(f'data/item-icons/{item}.png', filename="image.png")
+    embed.set_thumbnail(file=file)
+    # embed.set_thumbnail(url='attachment://image.png')
+    # embed.set_author(name="Author Name",
+    #                  icon_url="https://cdn.discordapp.com/attachments/520265639680671747/533389224913797122/rtgang.jpeg")
+    # embed.add_field(name="Field Name", value="Field Value", inline=False)
+    # embed.add_field(name="Field Name", value="Field Value", inline=True)
+    # embed.add_field(name="Field Name", value="Field Value", inline=True)
+
+    if auction.limit:
+        embed.set_footer(text=f'auction.limit')
+    return embed
+
+# -------------------------------------------------------------
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
 # -------------------------------------------------------------
 
-# Commands
+
 @bot.slash_command()
 async def auction(ctx):
     await asyncio.sleep(0)
+
 
 @auction.sub_command()
 async def create(ctx, item_sold: str, item_recv: str, intention: str, item_sold_amount: int, item_recv_amount: int, limit=None):
     if intention not in ['buy', 'sell']:
         await ctx.send('Intention must be either to `buy` or `sell`')
-    signal_service.add_auction(item_sold, item_recv, ctx.author, intention, item_sold_amount, item_recv_amount, limit)
+    signal_service.add_auction(item_sold, item_recv, ctx.author,
+                               intention, item_sold_amount, item_recv_amount, limit)
     await ctx.send('Auction created')
 
+
 @auction.sub_command()
-async def list(ctx, user: User=None):
+async def list(ctx, user: User = None):
     auctions = signal_service.list_auctions(user)
-    await ctx.send('Auctions: ' + str(auctions))
+    if user:
+        for auction in auctions:
+            await ctx.send(embed=create_embed(auction))
+    else:
+        await ctx.send('Auctions: ' + str(auctions))
+
 
 @bot.slash_command()
 async def info(ctx):
     await ctx.send('To be implemented')
 
+
 @bot.slash_command()
 async def buying(ctx):
     await ctx.send('To be implemented')
+
 
 @bot.slash_command()
 async def selling(ctx):
     await ctx.send('To be implemented')
 
+
 @bot.slash_command()
 async def remove(ctx):
     await ctx.send('To be implemented')
 
+
 @bot.slash_command()
 async def bid(ctx, user: disnake.User):
     await ctx.send('To be implemented')
+
 
 @bot.slash_command()
 async def source(ctx):
