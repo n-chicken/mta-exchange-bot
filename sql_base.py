6d9544c3437c2fb2df40f8326153e9b198d05ff1
n-chicken <nether_chicken@hotmail.com>
2022-01-25 09:44:37 -0300

    Use other port

--- sql_base.py
+++ sql_base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://mta:mta@127.0.0.1:3306/mta')
+engine = create_engine('mysql+pymysql://mta:mta@127.0.0.1:3307/mta')
 _SessionFactory = sessionmaker(bind=engine)
 
 Base = declarative_base()
